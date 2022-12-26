e675efe86293bf7a567675c4c80e4d0b2b87e5a9
Muhammad Sabir <mscsf20m001@pucit.edu.pk>
2022-12-26 15:28:20 +0500

    Helpoers files

---
+++ getCookiesFromReqest.php
@@ -0,0 +1,19 @@
+<?php
+
+// Get the cookies from the request
+function getCookies($headers)
+{
+    $cookies = [];
+    try {
+        $cookie = $headers['Set-Cookie'];
+        foreach ($cookie as $c) {
+            $c = explode(';', $c);
+            $cookies[] = $c[0];
+            //$c=explode('=',$c[0]);
+            //$cookies[$c[0]]=$c[1];
+        }
+        //while sending back cookie use headers named Cookie
+    } catch (\Throwable $th) {
+    }
+    return implode('; ',   $cookies);
+}
